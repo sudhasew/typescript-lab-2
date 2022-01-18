ec1fda7f94caf9ee69d3be6a1a5fea1261163e3e
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2022-01-18 14:05:12 -0500

    done with lab2

--- tests/HtmlGreeter.test.ts
+++ tests/HtmlGreeter.test.ts
@@ -1,14 +1,14 @@
 import { HtmlGreeter } from "../HtmlGreeter";
 describe("HtmlGreeter", () => {
-  test("It returns the <h1>Hello,Sudha!</h1> with default tagName", () => {
+  test("It returns the <h1>Hello,Sudha!</h1> with default tagName <h1>", () => {
     const htmlGreeter = new HtmlGreeter("Hello");
     expect(htmlGreeter.greet("Sudha")).toBe("<h1>Hello,Sudha!</h1>");
   });
-  test("It returns the <h1>Hello,Jaas!</h1>", () => {
+  test("It returns the Hello,Jaas! with default tagName <h1>", () => {
     const htmlGreeter = new HtmlGreeter("Hello");
     expect(htmlGreeter.greet("Jaas")).toBe("<h1>Hello,Jaas!</h1>");
   });
-  test("It returns the <h2>How are you doing,Jaas!</h2>", () => {
+  test("It returns the <h2>How are you doing,Nish!</h2>", () => {
     const htmlGreeter = new HtmlGreeter("How are you doing");
     htmlGreeter.tagName = "h2";
     expect(htmlGreeter.greet("Nish")).toBe("<h2>How are you doing,Nish!</h2>");
@@ -16,11 +16,13 @@ describe("HtmlGreeter", () => {
   test("It returns the <h3>How are you doing,Jaas!</h3>", () => {
     const htmlGreeter = new HtmlGreeter("How are you doing");
     htmlGreeter.tagName = "h3";
-    expect(htmlGreeter.greet("Nish")).toBe("<h3>How are you doing,Nish!</h3>");
+    expect(htmlGreeter.greet("Jaas")).toBe("<h3>How are you doing,Jaas!</h3>");
   });
-  test("It returns the <h5>How are you doing,Jaas!</h5>", () => {
+  test("It returns the <h5>How are you doing,Vyshnavi!</h5>", () => {
     const htmlGreeter = new HtmlGreeter("How are you doing");
     htmlGreeter.tagName = "h5";
-    expect(htmlGreeter.greet("Nish")).toBe("<h5>How are you doing,Nish!</h5>");
+    expect(htmlGreeter.greet("Vyshnavi")).toBe(
+      "<h5>How are you doing,Vyshnavi!</h5>"
+    );
   });
 });
